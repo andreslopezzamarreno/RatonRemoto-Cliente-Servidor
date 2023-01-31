909ed13973e1b99ec9092dbed1f67ae44dc881f6
Andrés López Zamarreño <lopez.zama@gmail.com>
2023-01-31 20:42:39 +0100

    update

--- EntregaFinal/RatonRemoto/src/main/java/com/example/entregafinalpsp/Usuario.kt
+++
@@ -1,3 +0,0 @@
-package com.example.entregafinalpsp
-
-data class Usuario(var usuario: String, var pass: String):java.io.Serializable
---
+++ Raton-Remoto/app/src/main/java/com/example/ratonremoto/Usuario.kt
@@ -0,0 +1,3 @@
+package com.example.ratonremoto
+
+data class Usuario(var usuario: String, var pass: String):java.io.Serializable
